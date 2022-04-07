824eb5a568f8eccc6f85f5542ada3621acd87362
Вячеслав <95027060+vyacheslav-79109734555@users.noreply.github.com>
2022-04-07 18:15:39 +0300

    Сумма факториалов

---
+++ _Домашняя работа/7_Наибольшая сумма цифр/7_Наибольшая сумма цифр.py
@@ -0,0 +1,17 @@
+print('вычисление будет выполнено при ENTER = 0')
+number = int(input('Введите число: '))
+max_result = 0
+max_number = 0
+while number != 0:
+    meaning = number
+    counter = 0
+    while number > 0:
+        counter += number % 10
+        number //= 10
+    if counter > max_result:
+        max_result = counter
+        max_number = meaning
+    number = int(input('Введите число: '))
+print('Максимальная сумма цифр:', max_result, 'находиться в числе:', max_number)
+
+# **************************
